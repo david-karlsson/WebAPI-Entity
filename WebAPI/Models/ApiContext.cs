9a0f1a910a7f2bde47b3a1c39631258802669a93
David Karlsson <david.karlsson1@learnet.se>
2020-03-03 15:32:51 +0100

    Add project files.

---
+++ WebAPI/Models/ApiContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAPI.Models
+{
+    public partial class ApiContext : DbContext
+    {
+        public ApiContext()
+        {
+        }
+
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+        }
+
+
+
+        public virtual DbSet<Customer>Customer { get;set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Customer>(entity =>
+
+            {
+
+                entity.Property(e => e.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+            }
+
+              );
+
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
